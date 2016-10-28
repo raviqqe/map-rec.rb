3eca926ad5be35f352a6d89f6879c5f116a42077
Yota Toyama <raviqqe@gmail.com>
2016-10-29 05:35:37 +0900

    Fix map_rec impl

--- lib/map-rec/map_rec.rb
+++ lib/map-rec/map_rec.rb
@@ -1,12 +1,14 @@
 def map_rec object, array_elem=true, hash_key=true, hash_value=true, &block
+  map = lambda { |o| map_rec(o, &block) }
+
   case object
   when Array
     object.map do |elem|
-      array_elem ? map_rec(elem) : elem
+      array_elem ? map.call(elem) : elem
     end
   when Hash
     object.map do |key, value|
-      [(hash_key ? map_rec(key) : key), (hash_value ? map_rec(value) : value)]
+      [(hash_key ? map.call(key) : key), (hash_value ? map.call(value) : value)]
     end.to_h
   else
     block.call object
