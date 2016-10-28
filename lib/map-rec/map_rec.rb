65220bac62f37cf16c37dac107b94d3378e04ab8
Yota Toyama <raviqqe@gmail.com>
2016-10-29 05:26:41 +0900

    Implement map_rec

--- lib/map-rec/map_rec.rb
+++ lib/map-rec/map_rec.rb
@@ -1,53 +1,14 @@
-require 'block-is-hash'
-
-
-
-def map_rec &block
-  repeats = %i(server load_module fastcgi_param set if_)
-
-  hash = block_is_hash repeats, &block
-
-  replace_elems! hash
-  replace_elems! repeats
-
-  Compiler.new(repeats).compile hash
-end
-
-
-def replace_elems! object
-  { if_: :if, return_: :return }.each do |origin, subst|
-    replace_elem! origin, subst, object
-  end
-end
-
-
-def replace_elem! origin, subst, object
-  replace = lambda { |o| replace_elem! origin, subst, o }
-
+def map_rec object, array_elem=true, hash_key=true, hash_value=true, &block
   case object
-  when Hash
-    new_pairs = {}
-
-    object.each do |key, value|
-      case origin
-      when key
-        object.delete key
-        new_pairs[subst] = value
-      when value
-        object[key] = subst
-      else
-        replace.call value
-      end
-    end
-
-    object.merge! new_pairs
   when Array
-    object.each_with_index do |value, key|
-      if value == origin
-        object[key] = subst
-      else
-        replace.call value
-      end
+    object.map do |elem|
+      array_elem ? map_rec(elem) : elem
     end
+  when Hash
+    object.map do |key, value|
+      [(hash_key ? map_rec(key) : key), (hash_value ? map_rec(value) : value)]
+    end.to_h
+  else
+    block.call object
   end
 end
